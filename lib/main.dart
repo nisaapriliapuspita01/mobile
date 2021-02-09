a0c175032439e1fe986135e26e99b7c1b90927a8
nisaapriliapuspita01 <nisaaprilia9d@gmail.com>
2021-02-09 04:56:19 -0800

    mobile

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),//ThemData
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text("Kawal Corona"),
+        ),
+       body: Center(
+         child: Text("Nisa Aprilia"),
+       ),
+      ),
+    );
+  }
+}
+
